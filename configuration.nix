49f2d2787069f667ec13bee2f764e8178cc1f595
MrFoX <65967000+FooqX@users.noreply.github.com>
2025-12-17 19:00:47 +0200

    Add initial NixOS configuration
    This configuration file sets up the NixOS system with various services, packages, and settings, including bootloader configuration, Bluetooth support, GNOME desktop environment, and NVIDIA graphics support.

---
+++ configuration.nix
@@ -0,0 +1,241 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel. _latest
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelParams = [
+  	"nvidia-drm.fbdev=1"
+  	
+  	"nowatchdog" # disable watchdog
+  	"nmi_watchdog=0" # disable watchdog
+  	"intel_idle.max_cstate=2" # less parking
+  	"threadirqs"
+  	"usbcore.autosuspend=-1" # no usb suspend
+  ];
+
+  # Blacklist watchdog kernel modules
+  boot.blacklistedKernelModules = [
+  	"iTCO_wdt"
+  	"watchdog"
+  ];
+  
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Riga";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "lv_LV.UTF-8";
+    LC_IDENTIFICATION = "lv_LV.UTF-8";
+    LC_MEASUREMENT = "lv_LV.UTF-8";
+    LC_MONETARY = "lv_LV.UTF-8";
+    LC_NAME = "lv_LV.UTF-8";
+    LC_NUMERIC = "lv_LV.UTF-8";
+    LC_PAPER = "lv_LV.UTF-8";
+    LC_TELEPHONE = "lv_LV.UTF-8";
+    LC_TIME = "lv_LV.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome.core-developer-tools.enable = true;
+
+  hardware.graphics = {
+    	enable = true;
+    	extraPackages = with pkgs; [
+    	    # Libva, nvidia-vaapi packages for hardware acceleration
+    		libva-vdpau-driver
+    		libvdpau-va-gl
+    		nvidia-vaapi-driver
+    		egl-wayland
+    	];
+  };
+  
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Disable CUPS
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account.
+  users.users.fox = {
+    isNormalUser = true;
+    createHome = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  security.sudo.enable = true;
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    curl
+    firefox
+    fish
+    fastfetch
+    freetype
+    fontconfig
+    flatpak
+    gcc15 # make sure to check
+    git
+    gnome-tweaks
+    gparted
+    inxi
+    jq
+    micro
+    p7zip
+    parted
+    pciutils
+    polkit_gnome
+    tree
+    tldr
+    unzip
+    usbutils
+    vlc
+    vulkan-tools
+    vulkan-validation-layers
+    wget
+    which
+    zip
+    xdg-desktop-portal-gtk
+
+    # codecs
+    gst_all_1.gstreamer
+    gst_all_1.gst-vaapi
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-base
+    libva
+    libva-utils
+    ffmpeg-full
+
+  ];
+
+  # Fonts setup
+  fonts.enableDefaultPackages = true;
+  fonts.packages = with pkgs; [
+  	noto-fonts
+    noto-fonts-cjk-sans
+    jetbrains-mono
+    nerd-fonts.hack
+    cantarell-fonts
+  ];
+  fonts.fontconfig.subpixel.rgba = "rgb";
+  fonts.fontconfig.subpixel.lcdfilter = "light";
+  
+  # Environment variables (NVIDIA/Wayland)
+  environment.variables = {
+  	LIBVA_DRIVER_NAME = "nvidia";
+  	GBM_BACKEND = "nvidia-drm";
+  	__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  	
+  	NIXOS_OZONE_WL = "1";
+  };
+
+  # NVIDIA setup
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+  	# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  	# Enable this if you have graphical corruption issues or application crashes after waking
+  	# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+  	# of just the bare essentials.
+  	powerManagement.enable = false;
+  	powerManagement.finegrained = false;
+  	open = true;
+  	nvidiaSettings = true;
+  	package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+  networking.enableIPv6 = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
